6768a9fd12ef09dad9c3f1a829bd2d9cf3c3bcfa
Wolk777 <Sokol.Vlad90@mail.ru>
2019-12-04 11:39:26 +0300

    fixed syntax errors, edited README.md

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 (function(){
-	let locationSearch; 
-	let numberPage; 
-	let arrayListings; 
+	let locationSearch;
+	let numberPage;
+	let arrayListings;
 	let favoriteList;
-
-	function init() {
-		locationSearch = null;
-		numberPage = null;
-		arrayListings = [];
-		favoriteList = [];
-
-		document.getElementById("modal-overlay").addEventListener("click",closeModal);
-		document.getElementById("btnSearch").addEventListener("click",hendlerSearch);
-		document.getElementById("heartFavorite").addEventListener("click", hendlerHeart);
-		document.getElementById("modal").addEventListener("click", hendlerClickModal);
-
-		if (window.localStorage.getItem("favorite")) {
-			favoriteList = JSON.parse(window.localStorage.getItem("favorite"));
-		} else {
-			window.localStorage.setItem("favorite", JSON.stringify(favoriteList));
-		}
-	}
-
+  
 	function openModal(typeModal) {
 		document.getElementById("modal").innerHTML = "";
 		document.getElementById("modal-overlay").classList.remove("modal_closed");
@@ -39,25 +21,29 @@
 		document.getElementById("modalLitle").innerHTML = `<p class="messageLitleModal">${str}<p>`;
 	}
 
-	function openModalFavorite() {
-		let favorite = JSON.parse(window.localStorage.getItem("favorite"));
-		
-		if (isEmptyObject(favorite)) {
-			document.getElementById("modal").innerHTML = `<p class="messageEmptyFavorite">The list of favorite ads is empty. 
-			Click on the heart to add an ad to your favorites.<p>`;
-			return;
-		}
-		
-		document.getElementById("modal").innerHTML = "";
-		for (let item of favorite) {
-			createListing(item, "#modal");
-		}
+	function findItem(arr, itemUrl) {
+		return arr.find(item => item.lister_url === itemUrl);
+	}
+
+	function saveToFavorites(url) {
+		favoriteList.push(findItem(arrayListings, url));
+		window.localStorage.setItem("favorite", JSON.stringify(favoriteList));
+	}
+
+	function removeFromFavorites(url) {
+		favoriteList.forEach((item, index) => {
+			if (item.lister_url === url) {
+				favoriteList.splice(index, 1)
+			}
+		})
+
+		window.localStorage.setItem("favorite", JSON.stringify(favoriteList));
 	}
 
 	function createListing(listing, selector) {
-		let url = listing.lister_url;
-		let classHeart = findItem(favoriteList, url) ? "fas" : "far";
-		let strContent = `
+		const url = listing.lister_url;
+		const classHeart = findItem(favoriteList, url) ? "fas" : "far";
+		const strContent = `
 		<div class="listing" data-url=${listing.lister_url}>
 			<figure class="thumb">
 				<img src=${listing.thumb_url} alt="photo of the ${listing.property_type}">
@@ -77,14 +63,51 @@
 		document.querySelector(selector).insertAdjacentHTML("beforeend", strContent);
 	}
 
+	function checkCodeError(code, text) {
+		const codes = ["200", "201", "202", "210", "500", "900", "901", "902", "910", "911"];
+
+		if (codes.includes(code)) {
+			openLitleModal(text[0].toUpperCase() + text.slice(1));
+			return;
+		}		
+
+		openLitleModal("Error loading data. Check the entered data and repeat the search.");
+	}
+
+	function checkResponse(response) {
+		const codeResponse = response.response.application_response_code;
+		const textResponse = response.response.application_response_text;
+		if (codeResponse < 100 || codeResponse > 199) {
+			checkCodeError(codeResponse, textResponse);
+			return;
+		}
+
+		const {listings} = response.response;
+		arrayListings = [...arrayListings,...listings];
+		createListListings(response);
+	}
+
+	function findLocation (location) {
+		fetch(`https://cors-anywhere.herokuapp.com/https://api.nestoria.co.uk/api?encoding=json&pretty=1
+			&action=search_listings&country=uk&listing_type=rent&page=${numberPage}&place_name=${location}`)
+	  .then(response => response.json())
+	  .then(result => checkResponse(result))
+	  .catch(() => openLitleModal("An error occurred while loading data. Please try the search again. We apologize."))
+	}
+
+	function loadListings() {
+		numberPage +=1;
+		findLocation (locationSearch);
+	}
+
 	function createModalListing(url, container) {
 		let strBedroom;
 		let strBathroom;
-		let listing = (container === "#containerMain") ? findItem(arrayListings, url) : findItem(favoriteList, url);
-		let separator = " &#183 ";
-		let classHeart = findItem(favoriteList, url) ? "fas" : "far";
-		let strKeywords = separator + listing.keywords.split(", ").join(separator);
-		let strSize = !listing.size ? "" : separator + listing.size + " sq.ft";
+		const listing = (container === "#containerMain") ? findItem(arrayListings, url) : findItem(favoriteList, url);
+		const separator = " &#183 ";
+		const classHeart = findItem(favoriteList, url) ? "fas" : "far";
+		const strKeywords = separator + listing.keywords.split(", ").join(separator);
+		const strSize = !listing.size ? "" : `${separator + listing.size  } sq.ft`;
 
 		if (!listing.bedroom_number) {
 			strBedroom = "";
@@ -97,8 +120,8 @@
 		}
 		const bathroomTitle = listing.bathroom_number === 1 ? " Bath" : " Baths";
 		strBathroom = `${separator} ${listing.bathroom_number} ${bathroomTitle}`;
-		
-		let strContent = `
+
+		const strContent = `
 			<div class="listingModal">
 				<div class="photo">
 					<img src=${listing.img_url} alt="photo of the ${listing.property_type}">
@@ -124,26 +147,8 @@
 		document.getElementById("modal").innerHTML = strContent;
 	}
 
-	function createListListings(response) {
-		if (numberPage === 1) {
-			document.querySelector("#main").innerHTML = `
-				<div class="containerMain" id="containerMain"></div>
-				<div class="btnLoad" id="btnLoad">
-					<button>Load more...</button>
-				</div>
-			`;
-			document.querySelector("#btnLoad").addEventListener("click", loadListings);
-			document.querySelector("#containerMain").addEventListener("click", checkTarget);
-		}
-
-		let listings = response.response.listings;
-		for (let listing of listings){
-			createListing(listing, "#containerMain");
-		}
-	}
-
 	function changeHeart(target) {
-		let url = target.dataset.url;
+		const {url} = target.dataset;
 
 		if (findItem(favoriteList, url)) {
 			target.classList.remove("fas");
@@ -156,8 +161,57 @@
 		}
 	}
 
-	function hendlerSearch() {
-		let location = document.getElementById("inputSearch").value;
+	function checkTarget(e) {
+	  e.preventDefault();
+    if (e.target.tagName === "H3") {
+    	const {url} = e.target.parentElement.parentElement.parentElement.dataset;
+    	openModal("modal");
+  		createModalListing(url, "#containerMain");
+    }
+    if (e.target.className.includes("fa-heart")) {
+			changeHeart(e.target);
+    }
+	}
+
+	function createListListings(response) {
+		if (numberPage === 1) {
+			document.querySelector("#main").innerHTML = `
+				<div class="containerMain" id="containerMain"></div>
+				<div class="btnLoad" id="btnLoad">
+					<button>Load more...</button>
+				</div>
+			`;
+			document.querySelector("#btnLoad").addEventListener("click", loadListings);
+			document.querySelector("#containerMain").addEventListener("click", checkTarget);
+		}
+
+		const {listings} = response.response;
+		listings.forEach(listing => createListing(listing, "#containerMain"));
+	}
+
+	function isEmptyObject(obj) {
+		const arrKeys = Object.keys(obj);
+		if(arrKeys.length === 0) {
+			return true;
+		}
+	  return false;
+	}
+
+	function openModalFavorite() {
+		const favorite = JSON.parse(window.localStorage.getItem("favorite"));
+		
+		if (isEmptyObject(favorite)) {
+			document.getElementById("modal").innerHTML = `<p class="messageEmptyFavorite">The list of favorite ads is empty. 
+			Click on the heart to add an ad to your favorites.<p>`;
+			return;
+		}
+		
+		document.getElementById("modal").innerHTML = "";
+		favorite.forEach(item => createListing(item, "#modal"));
+	}
+	
+	function handlerSearch() {
+		const location = document.getElementById("inputSearch").value;
 		
 		if (location === "") {
 			openLitleModal("Search fields are empty! Please enter a place to search!");
@@ -170,19 +224,19 @@
 		document.getElementById("inputSearch").value = "";
 		document.querySelector("#main").innerHTML = "";
 
-		findLocation(locationSearch, numberPage);	
+		findLocation(locationSearch);	
 	}
 
-	function hendlerHeart(e) {
+	function handlerHeart(e) {
 		e.preventDefault();
 		openModal("modal");
 		openModalFavorite();
 	}
 
-	function hendlerClickModal(e) {
+	function handlerClickModal(e) {
 	  e.preventDefault();
 	  if (e.target.tagName === "H3") {
-	  	let url = e.target.parentElement.parentElement.parentElement.dataset.url;
+	  	const {url} = e.target.parentElement.parentElement.parentElement.dataset;
 			createModalListing(url);
 	  }
 
@@ -192,11 +246,11 @@
 				openModalFavorite();
 			};
 			
-			let heartList = document.querySelector("#containerMain");
-			let url = e.target.dataset.url;
+			const heartList = document.querySelector("#containerMain");
+			const {url} = e.target.dataset;
 			if (heartList) {
-				let arrListing = heartList.querySelectorAll(".fa-heart");
-				for (let item of arrListing) {
+				const arrListing = heartList.querySelectorAll(".fa-heart");
+				arrListing.forEach(item => {
 					if (item.dataset.url === url) {
 						if (findItem(favoriteList, url)) {
 							item.classList.remove("far");
@@ -206,87 +260,27 @@
 							item.classList.add("far");
 						}
 					}
-				}
+				})
 			}
 		}
 	}
 
-	function findLocation (location, numberPage) {
-		fetch(`https://cors-anywhere.herokuapp.com/https://api.nestoria.co.uk/api?encoding=json&pretty=1
-			&action=search_listings&country=uk&listing_type=rent&page=${numberPage}&place_name=${location}`)
-	  .then(response => response.json())
-	  .then(result => checkResponse(result))
-	  .catch(err => openLitleModal("An error occurred while loading data. Please try the search again. We apologize."))
-	}
+	function init() {
+		locationSearch = null;
+		numberPage = null;
+		arrayListings = [];
+		favoriteList = [];
 
-	function checkResponse(response) {
-		let codeResponse = response.response.application_response_code;
-		let textResponse = response.response.application_response_text;
-		if (codeResponse < 100 || codeResponse > 199) {
-			checkCodeError(codeResponse, textResponse);
-			return;
+		document.getElementById("modal-overlay").addEventListener("click",closeModal);
+		document.getElementById("btnSearch").addEventListener("click",handlerSearch);
+		document.getElementById("heartFavorite").addEventListener("click", handlerHeart);
+		document.getElementById("modal").addEventListener("click", handlerClickModal);
+
+		if (window.localStorage.getItem("favorite")) {
+			favoriteList = JSON.parse(window.localStorage.getItem("favorite"));
+		} else {
+			window.localStorage.setItem("favorite", JSON.stringify(favoriteList));
 		}
-
-		let listings = response.response.listings;
-		arrayListings = [...arrayListings,...listings];
-		createListListings(response);
-	}
-
-	function checkCodeError(code, text) {
-		let codes = ["200", "201", "202", "210", "500", "900", "901", "902", "910", "911"];
-
-		if (codes.includes(code)) {
-			openLitleModal(text[0].toUpperCase() + text.slice(1));
-			return;
-		}		
-
-		openLitleModal("Error loading data. Check the entered data and repeat the search.");
-	}
-
-	function checkTarget(e) {
-	  e.preventDefault();
-    if (e.target.tagName === "H3") {
-    	let url = e.target.parentElement.parentElement.parentElement.dataset.url;
-    	openModal("modal");
-  		createModalListing(url, "#containerMain");
-    }
-    if (e.target.className.includes("fa-heart")) {
-			changeHeart(e.target);
-    }
-	}
-
-	function loadListings() {
-		numberPage ++;
-		findLocation (locationSearch, numberPage);
-	}
-
-	function saveToFavorites(url) {
-		favoriteList.push(findItem(arrayListings, url));
-		window.localStorage.setItem("favorite", JSON.stringify(favoriteList));
-	}
-
-	function removeFromFavorites(url) {
-		let indexRemove;
-		favoriteList.forEach((item, index) => {
-			if (item['lister_url'] === url) {
-				favoriteList.splice(index, 1)
-			}
-		})
-
-		window.localStorage.setItem("favorite", JSON.stringify(favoriteList));
-	}
-
-	function findItem(arr, itemUrl) {
-		return arr.find(item => item.lister_url === itemUrl);
-	}
-
-	function isEmptyObject(obj) {
-	  for (let i in obj) {
-	    if (obj.hasOwnProperty(i)) {
-	      return false;
-	    }
-	  }
-	  return true;
 	}
 
 	init();
